cb4cce253aa1014a56ca1ea13cc801cf7c9c4caf
Victor Murta Garcia <victor.murta@dinfor.uniceub>
2023-03-20 10:22:20 -0300

    projeto de profissao

---
+++ search/main.js
@@ -0,0 +1,13 @@
+
+
+function BuscarEspecializacao(buscar, selecionar){
+    const valorBuscado = buscar.value + "";
+    const valorFinal = valorBuscado.toLowerCase().split('');
+
+    valorFinal.forEach((elemento) => {
+        console.log(elemento);
+    })
+
+    
+}
+
